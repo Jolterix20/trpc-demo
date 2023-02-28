a3068ea10c84606ddcf024fe1c7161b12528ddfc
Jolterix20 <jayeshvbane@gmail.com>
2023-02-28 18:29:15 +0530

    nested router and zod import

---
+++ server/routers/users.ts
@@ -0,0 +1,12 @@
+import { t } from '../trpc'
+import { z } from 'zod'
+
+const userProcedure = t.procedure.input(z.object({ userId: z.string() }))
+
+export const userRouter = t.router({
+	get: userProcedure.query(({ input }) => {
+		return {
+			id: input.userId,
+		}
+	}),
+})
