725501c9d436a11bd3ed3cb15dc5b5ce2b1fe198
Jolterix20 <jayeshvbane@gmail.com>
2023-02-28 18:16:35 +0530

    code split into files

--- server/api.ts
+++ server/api.ts
@@ -1,31 +1,14 @@
 import express from 'express'
 import cors from 'cors'
 
-import { initTRPC } from '@trpc/server'
+import { appRouter } from './routers/index'
 import { createExpressMiddleware } from '@trpc/server/adapters/express'
 
-const t = initTRPC.create()
-
-const appRouter = t.router({
-	sayHi: t.procedure.query(() => {
-		return 'Hi'
-	}),
-	logToServer: t.procedure
-		.input((v) => {
-			if (typeof v === 'string') return v
-
-			throw new Error('Invalid input: Expected String')
-		})
-		.mutation((req) => {
-			console.log(`Client says: ${req.input}`)
-			return true
-		}),
-})
 const app = express()
 app.use(cors({ origin: 'http://localhost:5173' }))
 
 app.use('/trpc', createExpressMiddleware({ router: appRouter }))
 
-app.listen(3000)
+app.listen(3000, () => console.log(`Server running`))
 
 export type AppRouter = typeof appRouter
---
+++ server/trpc.ts
@@ -0,0 +1,4 @@
+import { initTRPC } from '@trpc/server'
+
+// This line should always be used just once
+export const t = initTRPC.create()
